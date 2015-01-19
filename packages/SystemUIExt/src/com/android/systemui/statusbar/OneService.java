2cf2efdceaad2c65f367752c97bf0d705dff1a49
karon <kameron19921016@yahoo.com>
2015-01-19 18:00:22 +0800

    frameworks:add OneService
    Change-Id: Icf9b6da50b1c34b23f4c4783a77b980356c9b470

---
+++ packages/SystemUIExt/src/com/android/systemui/statusbar/OneService.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015 The Android One Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.systemui.SystemUI;
+
+public class OneService extends SystemUI {
+    static final String TAG = "OneService";
+
+    private final Handler mHandler = new Handler();
+    private final Receiver mReceiver = new Receiver();
+
+    public void start() {
+        mReceiver.init();
+    }
+
+    private final class Receiver extends BroadcastReceiver {
+
+        public void init() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_TIME_TICK);
+        filter.addAction(Intent.ACTION_TIME_CHANGED);
+        filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
+        filter.addAction(Intent.ACTION_USER_SWITCHED);
+        mContext.registerReceiver(this, filter, null, mHandler);
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_TIME_TICK)) {
+                // Push Service Init ...
+            }
+        }
+    };
+}
+
