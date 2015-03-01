c62c47de2aad7717f3535e484a1d19ff41bc9c99
karon <kameron19921016@yahoo.com>
2015-03-01 17:09:47 +0800

    Frameworks: better resource call.
    Change-Id: Iab1ebfd789e0de84dc43e24b5e435659b45204e3

--- core/java/com/android/internal/util/one/utils/OneUtils.java
+++ core/java/com/android/internal/util/one/utils/OneUtils.java
@@ -33,15 +33,13 @@ import android.graphics.Color;
 import android.text.TextUtils;
 import java.util.Locale;
 
+import com.android.internal.R;
+
 /**
 * @hide
 */
 
 public class OneUtils {
-    public static final String[] BACKGROUND_SPECTRUM = { "#212121", "#27232e", "#2d253a",
-            "#332847", "#382a53", "#3e2c5f", "#442e6c", "#393a7a", "#2e4687", "#235395", "#185fa2",
-            "#0d6baf", "#0277bd", "#0d6cb1", "#1861a6", "#23569b", "#2d4a8f", "#383f84", "#433478",
-            "#3d3169", "#382e5b", "#322b4d", "#2c273e", "#272430" };
 
     public static boolean isSupportLanguage(boolean excludeTW) {
         Configuration configuration = Resources.getSystem().getConfiguration();
@@ -109,6 +107,8 @@ public class OneUtils {
     }
 
     public static int getCurrentHourColor() {
+        Resources res = Resources.getSystem();
+        final String[] BACKGROUND_SPECTRUM = res.getStringArray(R.array.color_animation);
         final int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         return Color.parseColor(BACKGROUND_SPECTRUM[hourOfDay]);
     }
